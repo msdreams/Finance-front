d48547f4108c335d71c7f2129caa887e956f4d17
Den <clixqqk@gmail.com>
2024-10-01 23:29:14 +0300

    header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,15 +5,15 @@ export const Header = () => {
     <header className="header">
       <div className="header__logo">
         <img src="/Finance-front/img/Logo(Nav).svg" alt="img" />
-        <p>MONETA</p>
+        <p className='header__logo--text'>MONETA</p>
       </div>
 
       <nav className='header__nav'>
-        <div>Home</div>
+        <div>Budget</div>
+        <div>History</div>
+        <div>Report</div>
         <div>About</div>
-        <div>Features</div>
-        <div>Contacts</div>
-        <div>Login</div>
+        <div className='header__nav--login-or-name'>Login</div>
       </nav>
     </header>
   )
