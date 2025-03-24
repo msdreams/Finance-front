421e4dcd95ce78ea80e6b0f87d799a077d042f6b
Maria Shmakova <masha.shmakova@gmail.com>
2025-03-24 14:14:12 +0200

    not found page were added

--- src/pages/PasswordUpdate.tsx
+++ src/pages/PasswordUpdate.tsx
@@ -3,7 +3,7 @@ export const PasswordUpdate = () => {
     <div className="flex px-4 min-h-screen w-full bg-background font-sans font-semibold text-primary-700 text-2xl">
       <div
         className="mx-auto mt-32 p-6 border-1 border-primary rounded-3xl h-fit">
-        Password successfully reset! <br /> Check your email and log in with the password from the email.
+        Password was successfully reset! <br /> Check your email and log in with the password from the email.
       </div>
     </div>
   )
